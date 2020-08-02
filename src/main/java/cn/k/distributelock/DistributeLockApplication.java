08163a652bf3668f9827225952edbb3687534179
googlebrain@163.com <googlebrain@163.com>
2020-08-02 21:38:23 +0800

    分布式锁的用法

--- src/main/java/cn/k/distributelock/DistributeLockApplication.java
+++ src/main/java/cn/k/distributelock/DistributeLockApplication.java
@@ -1,7 +1,12 @@
 package cn.k.distributelock;
 
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
@@ -10,6 +15,13 @@ public class DistributeLockApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DistributeLockApplication.class, args);
+    }
+
+    @Bean(initMethod = "start",destroyMethod = "close")
+    public CuratorFramework getCuratorFramework(){
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+        CuratorFramework client = CuratorFrameworkFactory.newClient("localhost:2181", retryPolicy);
+        return client;
     }
 
 }
---
+++ src/main/java/cn/k/distributelock/controller/CuratorController.java
@@ -0,0 +1,41 @@
+package cn.k.distributelock.controller;
+
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
+
+@RestController
+public class CuratorController {
+
+    @Resource
+    private CuratorFramework client;
+
+    @RequestMapping("/curator")
+    public String curator() {
+        System.out.println("进入了方法......");
+        InterProcessMutex lock = new InterProcessMutex(client, "/order");
+        try {
+            if (lock.acquire(3, TimeUnit.SECONDS)) {
+                try {
+                    System.out.println("我获得了锁。。。。。。。。。");
+                    Thread.sleep(10000);
+                } finally {
+                    System.out.println("释放了锁。。。。。");
+                    lock.release();
+                }
+            } else {
+                System.out.println("获取锁失败.......");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "完成";
+    }
+}
